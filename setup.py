18000d9b60e74a5a9df114cf0c71958cf5518975
Jernej Vivod <vivod.jernej@gmail.com>
2022-04-23 22:31:35 +0200

    [chore][#7] Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vertex-covering-approximations',
+    version='0.1.0',
+    packages=['vertex_covering_approximations', 'vertex_covering_approximations.tests', 'vertex_covering_approximations.algorithms', 'vertex_covering_approximations.visualization'],
+    url='https://github.com/jernejvivod/graph-covering-algorithms',
+    license='',
+    author='Jernej Vivod',
+    author_email='vivod.jernej@gmail.com',
+    description='Implementations of some vertex covering algorithms'
+)
